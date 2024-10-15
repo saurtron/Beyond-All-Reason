8f659fc42c03e944824063563e803be871692dbf
Saurtron <saurtron@riseup.net>
2024-10-15 13:45:12 +0200

    Rename game_map_ping.lua to game_map_point.lua.

--- luarules/gadgets/game_map_point.lua
+++ luarules/gadgets/game_map_point.lua
@@ -1,8 +1,8 @@
 
 function gadget:GetInfo()
 	return {
-		name    = "Game Map Ping",
-		desc	= 'allow sending map pings using the i18n library so everyone can see them in their own language',
+		name    = "Game Map Point",
+		desc	= 'allow sending map points using the i18n library so everyone can see them in their own language',
 		author	= 'Saurtron',
 		date	= 'Oct 2024',
 		license	= 'GNU GPL, v2 or later',
@@ -11,9 +11,9 @@ function gadget:GetInfo()
 	}
 end
 
--- for example usecase see the ping_wheel widget createMapPing and MapPingEvent methods
+-- for example usecase see the ping_wheel widget createMapPoint and MapPointEvent methods
 
-local PACKET_HEADER = "ping:"
+local PACKET_HEADER = "mppnt:"
 local PACKET_HEADER_LENGTH = string.len(PACKET_HEADER)
 
 if gadgetHandler:IsSyncedCode() then
@@ -22,27 +22,27 @@ if gadgetHandler:IsSyncedCode() then
 		if string.sub(msg, 1, PACKET_HEADER_LENGTH) ~= PACKET_HEADER then
 			return
 		end
-		SendToUnsynced("sendMapPing", playerID, string.sub(msg, PACKET_HEADER_LENGTH+1))
+		SendToUnsynced("sendMapPoint", playerID, string.sub(msg, PACKET_HEADER_LENGTH+1))
 		return true
 	end
 
 else	-- UNSYNCED
 
-	local function sendMapPing(_, playerID, msg)
+	local function sendMapPoint(_, playerID, msg)
 		local name,_,spec,_,playerAllyTeamID = Spring.GetPlayerInfo(playerID)
 		local mySpec = Spring.GetSpectatingState()
 		if playerAllyTeamID == Spring.GetMyAllyTeamID() or mySpec then
-			if Script.LuaUI("MapPingEvent") then
-				Script.LuaUI.MapPingEvent(playerID, msg)
+			if Script.LuaUI("MapPointEvent") then
+				Script.LuaUI.MapPointEvent(playerID, msg)
 			end
 		end
 	end
 
 	function gadget:Initialize()
-		gadgetHandler:AddSyncAction("sendMapPing", sendMapPing)
+		gadgetHandler:AddSyncAction("sendMapPoint", sendMapPoint)
 	end
 	function gadget:Shutdown()
-		gadgetHandler:AddSyncAction("sendMapPing")
+		gadgetHandler:AddSyncAction("sendMapPoint")
 	end
 end
 
