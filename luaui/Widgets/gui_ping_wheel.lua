532c07143531f1c44e3b6d6a4b128cacb4e87464
Saurtron <saurtron@riseup.net>
2024-10-17 15:26:03 +0200

    Fix some wrong code spacing.

--- luaui/Widgets/gui_ping_wheel.lua
+++ luaui/Widgets/gui_ping_wheel.lua
@@ -942,8 +942,8 @@ end
 local function drawCircleOutline(r, arr, hole)
     local function Circle()
         local vn = areaVertexNumber-1
-	local holeStart = (hole-1)*vn+1
-	local holeEnd = hole*vn
+        local holeStart = (hole-1)*vn+1
+        local holeEnd = hole*vn
         for i=1+holeEnd, #arr do
             glVertex(arr[i][1]*r, arr[i][2]*r)
         end
