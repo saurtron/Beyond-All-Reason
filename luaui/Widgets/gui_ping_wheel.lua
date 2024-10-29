b38bf065e4fda0de37a20fcb8bf4c349c95d9adc
Saurtron <saurtron@riseup.net>
2024-10-29 16:17:08 +0100

    Cleanup item dlists when setting color mode.

--- luaui/Widgets/gui_ping_wheel.lua
+++ luaui/Widgets/gui_ping_wheel.lua
@@ -729,7 +729,7 @@ function widget:Initialize()
     end
     WG['pingwheel_gui'].setUseColors = function(value)
         use_colors = value
-        destroyItemsDlist()
+        destroyItemsDlists()
     end
 
     addStandaloneSettings()
