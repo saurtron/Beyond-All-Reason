632e79633e81775fc579f7adea7d450997fffb43
Saurtron <saurtron@riseup.net>
2024-12-18 01:51:23 +0100

    Fix conflict.

--- luaui/Widgets/Tests/mex-building/pregame_mex_queue.lua
+++ luaui/Widgets/Tests/mex-building/pregame_mex_queue.lua
@@ -1,25 +1,15 @@
-<<<<<<< HEAD
-=======
 function skip()
 	return Spring.GetGameFrame() > 0
 end
 
->>>>>>> master
 -- Test whether mexes are able to clear queued buildings by shift-clicking
 function setup()
 	Test.clearMap()
 
-<<<<<<< HEAD
 	widget_cmd_extractor_snap = widgetHandler:FindWidget("Extractor Snap (mex/geo)")
 	assert(widget_cmd_extractor_snap)
 
 	widget_gui_pregame_build = widgetHandler:FindWidget("Pregame Queue")
-=======
-	local widget_cmd_extractor_snap = widgetHandler:FindWidget("Extractor Snap (mex/geo)")
-	assert(widget_cmd_extractor_snap)
-
-	local widget_gui_pregame_build = widgetHandler:FindWidget("Pregame Queue")
->>>>>>> master
 	assert(widget_gui_pregame_build)
 
 	WG['pregame-build'].setBuildQueue({})
@@ -44,15 +34,6 @@ function cleanup()
 	Spring.SetCameraState(initialCameraState)
 end
 
-<<<<<<< HEAD
-function test()
-	mexUnitDefId = UnitDefNames["armmex"].id
-	metalSpots = WG['resource_spot_finder'].metalSpotsList
-
-	midX, midZ = Game.mapSizeX / 2, Game.mapSizeZ / 2
-	targetMex = nil
-	targetMexDistance = 1e20
-=======
 -- tests both pregame mex snap behavior, as well as basic queue and blueprint handling
 function test()
 	local mexUnitDefId = UnitDefNames["armmex"].id
@@ -61,7 +42,6 @@ function test()
 	local midX, midZ = Game.mapSizeX / 2, Game.mapSizeZ / 2
 	local targetMex = nil
 	local targetMexDistance = 1e20
->>>>>>> master
 	for i = 1, #metalSpots do
 		local distance2 = math.distance2dSquared(midX, midZ, metalSpots[i].x, metalSpots[i].z)
 		if distance2 < targetMexDistance then
@@ -72,13 +52,9 @@ function test()
 
 	-- Place a mex off of a mex spot - expect mex snap to position it on the spot, as close as possible to cursor position
 	WG["pregame-build"].setPreGamestartDefID(mexUnitDefId)
-<<<<<<< HEAD
-	sx, sy, sz = Spring.WorldToScreenCoords(targetMex.x - 200, targetMex.y, targetMex.z - 200)
-=======
 	local activeBlueprint = WG["pregame-build"].getPreGameDefID()
 	assert(activeBlueprint == mexUnitDefId, "Active blueprint should be armmex")
 	local sx, sy, sz = Spring.WorldToScreenCoords(targetMex.x - 200, targetMex.y, targetMex.z - 200)
->>>>>>> master
 	Spring.WarpMouse(sx, sy)
 
 	-- wait for widgets to respond
@@ -95,11 +71,7 @@ function test()
 		targetMex.z
 	) < 100)
 
-<<<<<<< HEAD
-	snappedPosition = table.copy(WG.ExtractorSnap.position)
-=======
 	local snappedPosition = table.copy(WG.ExtractorSnap.position)
->>>>>>> master
 
 	-- queue the mex build
 	Script.LuaUI.MousePress(sx, sy, 1)
@@ -129,16 +101,10 @@ function test()
 
 	-- clear blueprint
 	WG["pregame-build"].setPreGamestartDefID(nil)
-<<<<<<< HEAD
-
-	-- Did the mex get de-queued?
-	buildQueue = WG['pregame-build'].getBuildQueue()
-=======
 	activeBlueprint = WG["pregame-build"].getPreGameDefID()
 	assert(activeBlueprint == nil, "Active blueprint should be nil")
 
 	-- Did the mex get de-queued?
 	local buildQueue = WG['pregame-build'].getBuildQueue()
->>>>>>> master
 	assert(#buildQueue == 0, "Build queue should be empty")
 end
