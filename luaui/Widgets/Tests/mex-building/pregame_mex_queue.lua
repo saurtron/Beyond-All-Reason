d23529ace2eec67ee7c3cac2ef1df99c79a7a784
Saurtron <saurtron@riseup.net>
2024-12-20 01:31:20 +0100

    Master has a local here.

--- luaui/Widgets/Tests/mex-building/pregame_mex_queue.lua
+++ luaui/Widgets/Tests/mex-building/pregame_mex_queue.lua
@@ -6,10 +6,10 @@ end
 function setup()
 	Test.clearMap()
 
-	widget_cmd_extractor_snap = widgetHandler:FindWidget("Extractor Snap (mex/geo)")
+	local widget_cmd_extractor_snap = widgetHandler:FindWidget("Extractor Snap (mex/geo)")
 	assert(widget_cmd_extractor_snap)
 
-	widget_gui_pregame_build = widgetHandler:FindWidget("Pregame Queue")
+	local widget_gui_pregame_build = widgetHandler:FindWidget("Pregame Queue")
 	assert(widget_gui_pregame_build)
 
 	WG['pregame-build'].setBuildQueue({})
