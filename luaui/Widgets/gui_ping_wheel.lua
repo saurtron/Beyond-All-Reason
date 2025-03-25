8041fba06d1ba804c6957029e3840c21addd6e84
Saurtron <saurtron@riseup.net>
2025-03-25 11:30:24 +0100

    luashader moved to new location.

--- luaui/Widgets/gui_ping_wheel.lua
+++ luaui/Widgets/gui_ping_wheel.lua
@@ -370,7 +370,7 @@ end
 
 -- Shader globals
 local shader
-local luaShaderDir = "LuaUI/Widgets/Include/"
+local luaShaderDir = "LuaUI/Include/"
 local LuaShader = VFS.Include(luaShaderDir.."LuaShader.lua")
 
 -- GL speedups
